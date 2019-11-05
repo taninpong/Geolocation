a3f0b0ea02aa226e5f69034e6278758dc3b77f8f
thaninpong <thaninpong@perfenterprise.com>
2019-11-05 17:13:10 +0700

    SS 17.13

--- geolocation/geolocation/geolocation.iOS/GetGPS.cs
+++ geolocation/geolocation/geolocation.iOS/GetGPS.cs
@@ -37,7 +37,7 @@ namespace geolocation.iOS
             var data = CLLocationManager.LocationServicesEnabled;
             if (data == false)
             {
-                var url = new NSUrl($"app-settings:LOCATION_SERVICES");
+                var url = new NSUrl($"app-settings:privacy-location");
                 UIApplication.SharedApplication.OpenUrl(url);
                 //var url = new NSUrl("App-Prefs:root=PRIVACY&path=LOCATION_SERVICES");
                 //UIApplication.SharedApplication.OpenUrl(url);
